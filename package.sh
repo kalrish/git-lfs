b479ff91ce3a6f462e11bb20a307a934901b3f95
David Joaquín Shourabi Porcel <david@djsp.eu>
2020-07-02 01:35:06 +0200

    FIXME

--- package.sh
+++ package.sh
@@ -1,3 +1,12 @@
+deployment_bucket="$1"
+
+git_commit_id="$(
+	git \
+		rev-parse \
+		HEAD:glawit \
+		#
+)"
+
 rm \
 	--force \
 	--recursive \
@@ -10,6 +19,8 @@ mkdir \
 	-- \
 	glawit_deployment_package \
 	#
+
+shift
 
 for python_version in "$@"
 do
@@ -76,10 +87,34 @@ cd \
 	glawit_deployment_package \
 	#
 
+deployment_package_file_path="${git_commit_id}.zip"
+
 zip \
 	--recurse-paths \
 	-9 \
-	../glawit_deployment_package.zip \
+	"${deployment_package_file_path}" \
 	-- \
 	python \
 	#
+
+deployment_object_key="glawit/${git_commit_id}.zip"
+
+mime_type="$(
+	file \
+		--brief \
+		--mime-type \
+		-- \
+		"${deployment_package_file_path}" \
+		#
+)"
+
+aws \
+	s3api \
+	put-object \
+	--bucket "${deployment_bucket}" \
+	--key "${deployment_object_key}" \
+	--body "${deployment_package_file_path}" \
+	--content-type application/zip \
+	#
+
+echo "${deployment_object_key}" > object_key
