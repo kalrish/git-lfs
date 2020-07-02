a37a8a0891c5a9f52b72c737db4ea06ae72047d4
David Joaquín Shourabi Porcel <david@djsp.eu>
2020-07-02 17:12:01 +0200

    fix Lambda layer packaging script

--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
 deployment_bucket="$1"
 
+output_directory=.glawit_deployment_package
+
 git_commit_id="$(
 	git \
 		rev-parse \
@@ -11,83 +13,35 @@ rm \
 	--force \
 	--recursive \
 	-- \
-	glawit_deployment_package \
-	glawit_deployment_package.zip \
+	"${output_directory}" \
 	#
 
 mkdir \
 	-- \
-	glawit_deployment_package \
+	"${output_directory}" \
 	#
 
 shift
 
-for python_version in "$@"
-do
-	container_id="$(
-		docker \
-			run \
-			--interactive \
-			--tty \
-			--detach \
-			--volume "${PWD}/glawit:/var/task:ro" \
-			-- \
-			"lambci/lambda:build-python${python_version}" \
-			/bin/sh \
-			#
-	)"
-
-	for package in core interface_apigw
-	do
-		docker \
-			exec \
-			--workdir /var/task \
-			-- \
-			"${container_id}" \
-			pip \
-			wheel \
-			--no-deps \
-			--build /tmp/build \
-			--wheel-dir /tmp/whl \
-			--progress-bar off \
-			-- \
-			"./${package}" \
-			#
-	done
-
-	docker \
-		exec \
-		-- \
-		"${container_id}" \
-		sh \
-		-c \
-		"pip install --target /tmp/deployment_package/python/lib/python${python_version}/site-packages/ /tmp/whl/*" \
-		#
-
-	docker \
-		cp \
-		-- \
-		"${container_id}:/tmp/deployment_package/." \
-		glawit_deployment_package \
-		#
-
-	docker \
-		container \
-		stop \
-		-- \
-		"${container_id}"
-
-	docker \
-		rm \
-		-- \
-		"${container_id}"
-done
+sh \
+	-- \
+	glawit/interface-apigw/package.sh \
+	"${output_directory}" \
+	"$@" \
+	#
 
 cd \
-	glawit_deployment_package \
+	"${output_directory}" \
 	#
 
 deployment_package_file_path="${git_commit_id}.zip"
+
+rm \
+	--force \
+	--recursive \
+	-- \
+	"${deployment_package_file_path}" \
+	#
 
 zip \
 	--recurse-paths \
@@ -114,7 +68,7 @@ aws \
 	--bucket "${deployment_bucket}" \
 	--key "${deployment_object_key}" \
 	--body "${deployment_package_file_path}" \
-	--content-type application/zip \
+	--content-type "${mime_type}" \
 	#
 
 echo "${deployment_object_key}" > object_key
